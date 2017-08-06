d9bb92a4dba0bd23fa225662658eeafac4baf59a
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-08-06 17:54:53 -0500

    Added styling and api for mars photos. Captions, auto rotate, arrow buttoms, consistent sizes added

--- main.js
+++ main.js
@@ -1,39 +1,48 @@
-$().ready(startTheShow);
+$().ready(getIt)
 
-function jsonFlickrFeed(json) {
-    $.each(json.items, function(i, item) {
-        let title = item.title.split(',', 1).pop();
-            console.log(title);
-            console.log(item.media.m);
 
-        if(i === 0) {
+function getIt() {
+    console.log('gettin it')
+    $.ajax({
+        url: "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&api_key=Bwso1Aw437t1qPzFl5KObyIsxdGjSxM5Z4JyYk6D",
+
+    }).done(function (data) {
+        parseIt(data)
+    })
+
+}
+
+
+function parseIt(data) {
+    console.log(data.photos)
+    $.each(data.photos, (key, value) => {
+        console.log(value)
+
+        if(key === 0) {
             $('#images').append(`
-                <div class="item active"><img src="${item.media.m}" alt="${item.title}">
+                <div class="item active"><img src="${value.img_src}" alt="Mars Rover Camera ${value.camera.full_name} image">
                     <div class="carousel-caption">
-                        <h6>${title}</h6>
-                        <!--<p></p>-->
+                        <h6>${value.camera.full_name}</h6>
+                        <p>${value.earth_date}</p>
                     </div>
                 </div>`)
+
+            $(".carousel-indicators").append(`
+                <li data-target="#myCarousel" data-slide-to="${key}" class="active"></li>`)
         }else{
             $('#images').append(`
-                <div class="item"><img src="${item.media.m}" alt="${item.title}">
+                <div class="item "><img src="${value.img_src}" alt="Mars Rover Camera ${value.camera.full_name} image">
                     <div class="carousel-caption">
-                        <h6>${title}</h6>
-                        <!--<p></p>-->
+                        <h6>${value.camera.full_name}</h6>
+                        <p>${value.earth_date}</p>
                     </div>
                 </div>`)
-            }
+
+            $(".carousel-indicators").append(`
+                <li data-target="#myCarousel" data-slide-to="${key}"></li>`)
+        }
 
 
     })
+    }
 
-
-}
-
-function startTheShow() {
-    $.ajax({
-        url: 'https://api.flickr.com/services/feeds/photos_public.gne',
-        dataType: 'jsonp',
-        data: { "id": "8623220@N02", "tags": "child labor, labor", "format": "json" }
-    });
-}
