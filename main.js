f4f0a5891c1a8f2e34709e63228db7e8e7298739
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-08-05 21:09:28 -0500

    Slideshow is loading and playing from API

--- main.js
+++ main.js
@@ -1,34 +1,36 @@
-$().ready(handleButtonClick);
+$().ready(startTheShow);
 
 function jsonFlickrFeed(json) {
-    // console.log(json);
-
     $.each(json.items, function(i, item) {
-        console.log(json.items)
-        // console.log(i)
-        console.log(item)
-        let title = item.title.split(',', 1).pop()
-            console.log(title)
+        let title = item.title.split(',', 1).pop();
+            console.log(title);
+            console.log(item.media.m);
+
+        if(i === 0) {
+            $('#images').append(`
+                <div class="item active"><img src="${item.media.m}" alt="${item.title}">
+                    <div class="carousel-caption">
+                        <h6>${title}</h6>
+                        <!--<p></p>-->
+                    </div>
+                </div>`)
+        }else{
+            $('#images').append(`
+                <div class="item"><img src="${item.media.m}" alt="${item.title}">
+                    <div class="carousel-caption">
+                        <h6>${title}</h6>
+                        <!--<p></p>-->
+                    </div>
+                </div>`)
+            }
 
 
-        let images = `<img src="${item.media.m}" alt="${item.title}">
-             <div class="carousel-caption">
-                <h3>${title}</h3>
-                <!--<p></p>-->
-              </div>`
-
-            $('#images').html(images)
-        })
+    })
 
 
 }
 
-        // $("<img />").attr("src", item.media.m).appendTo(".carousel-inner");
-
-
-function handleButtonClick() {
-    $("button").remove();
-
+function startTheShow() {
     $.ajax({
         url: 'https://api.flickr.com/services/feeds/photos_public.gne',
         dataType: 'jsonp',
