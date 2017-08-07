9351c1f3500e417b0094bb8ec83063c8c1656e7f
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-08-06 19:17:09 -0500

    Exercise complete, ready for review

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-$().ready(getIt)
+$().ready(getIt);
 
 
 function getIt() {
-    console.log('gettin it')
+    console.log('gettin it');
     $.ajax({
         url: "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&api_key=Bwso1Aw437t1qPzFl5KObyIsxdGjSxM5Z4JyYk6D",
 
@@ -14,9 +14,9 @@ function getIt() {
 
 
 function parseIt(data) {
-    console.log(data.photos)
+    console.log(data.photos);
     $.each(data.photos, (key, value) => {
-        console.log(value)
+        console.log(value);
 
         if(key === 0) {
             $('#images').append(`
@@ -25,7 +25,7 @@ function parseIt(data) {
                         <h6>${value.camera.full_name}</h6>
                         <p>${value.earth_date}</p>
                     </div>
-                </div>`)
+                </div>`);
 
             $(".carousel-indicators").append(`
                 <li data-target="#myCarousel" data-slide-to="${key}" class="active"></li>`)
@@ -36,13 +36,21 @@ function parseIt(data) {
                         <h6>${value.camera.full_name}</h6>
                         <p>${value.earth_date}</p>
                     </div>
-                </div>`)
+                </div>`);
 
             $(".carousel-indicators").append(`
                 <li data-target="#myCarousel" data-slide-to="${key}"></li>`)
         }
-
-
     })
-    }
+}
 
+$('.fade-button').click(function () {
+    console.log('fade button')
+    $('#myCarousel').toggleClass('carousel-fade')
+})
+
+$(".container").hover(function(){
+    $('.carousel-caption').show();
+}, function(){
+    $('.carousel-caption').hide();
+});
